8fd1c84167f43bdcf4cf7eba5c3c65a014f7e2e9
levanluc <levanluc95bk@gmail.com>
2022-06-08 09:50:45 +0700

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,34 +1,19 @@
-export interface Product {
-  id: number;
-  name: string;
-  price: number;
-  description: string;
-}
-
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
+    name: 'Phone 13 mini',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A largee phone with one of the best screens',
   },
   {
     id: 2,
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
+    name: 'Phone 13 plus',
+    descriptias: 'ADFADF',
   },
   {
     id: 3,
-    name: 'Phone Standard',
+    name: 'Phone Stander',
     price: 299,
-    description: ''
-  }
+    description: '',
+  },
 ];
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
